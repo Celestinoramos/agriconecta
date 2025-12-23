9f5f2d176529647c5a8aceaab91d1b9d115df36d
Celestinoramos <celestinocumbica03@gmail.com>
2025-12-23 13:20:51 +0100

    test

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,5 @@
 import type { NextRequest } from 'next/server'
 import { NextResponse } from 'next/server'
-import { createServerClient } from '@supabase/auth-helpers-nextjs'
 
 export async function middleware(request: NextRequest) {
   let response = NextResponse.next({
@@ -8,26 +7,7 @@ export async function middleware(request: NextRequest) {
       headers: request.headers,
     },
   })
-
-  const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL! ,
-    process.env. NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        getAll() {
-          return request.cookies. getAll()
-        },
-        setAll(cookiesToSet) {
-          cookiesToSet.forEach(({ name, value, options }) => {
-            response.cookies.set(name, value, options)
-          })
-        },
-      },
-    }
-  )
-
-  // const { data } = await supabase.auth.getSession()
-
+  
   return response
 }
 
